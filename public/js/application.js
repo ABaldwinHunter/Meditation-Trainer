1428401944a20354ec5c23c206779d2e708980d1
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-26 02:14:34 -0500

    adjust

--- public/js/application.js
+++ public/js/application.js
@@ -8,12 +8,6 @@ $(document).ready(function() {
     $target.toggleClass('soft');
   });
 
-  $('#lung').hover(function(event){
-    var $target = $(event.target);
-    // $target.show().animate({ top: 305 }, {duration: 1000, easing: 'easeOutBounce'});
-    $target.effect("bounce", { direction:'down', times:5 }, 300);
-  });
-
   $('.exit_button').click(function(){
     meditation.isOver = true;
     meditation.checkFinish();
